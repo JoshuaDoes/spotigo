f3d812840080dfd0865b81e354ba77d7ca889c93
JoshuaDoes <joshuadoespersonalstuff@gmail.com>
2018-10-15 02:40:47 -0400

    Add search support and return data with track ID if an error occurs

--- spotigo.go
+++ spotigo.go
@@ -27,7 +27,7 @@ type SpotigoTrackInfo struct {
 	Name        string           `json:"name"`        //Track name
 	TrackNumber int              `json:"number"`      //Track number
 	DiscNumber  int              `json:"disc_number"` //Disc number
-	Duration    int              `json:"duration"`    //Track duration
+	Duration    int              `json:"duration"`    //Track duration in milliseconds
 	Album       SpotigoAlbumInfo `json:"album"`       //Album
 }
 type SpotigoTrackOEmbed struct {
@@ -48,6 +48,53 @@ type SpotigoDate struct {
 	Day   int
 }
 
+type SpotigoSearch struct {
+	Results SpotigoSearchResults `json:"results"` //Search results
+}
+type SpotigoSearchResults struct {
+	Tracks SpotigoSearchTracks `json:"tracks"` //Tracks
+}
+type SpotigoSearchTracks struct {
+	Hits []SpotigoSearchHit `json:"hits"` //Hits
+}
+type SpotigoSearchHit struct {
+	Album    SpotigoSearchHitAlbum    `json:"album"`
+	Artists  []SpotigoSearchHitArtist `json:"artists"`
+	ImageURL string                   `json:"image"`
+	Name     string                   `json:"name"`
+	URI      string                   `json:"uri"`
+	Duration int                      `json:"duration"`
+}
+type SpotigoSearchHitAlbum struct {
+	//Artists //Unknown data type
+	ImageURL string `json:"image"`
+	Name     string `json:"name"`
+	URI      string `json:"uri"`
+}
+type SpotigoSearchHitArtist struct {
+	ImageURL string `json:"image"`
+	Name     string `json:"name"`
+	URI      string `json:"uri"`
+}
+
+func (c *Client) SearchTracks(query string) (*SpotigoSearchTracks, error) {
+	query = url.QueryEscape(query)
+
+	searchJSON, err := http.Get(fmt.Sprintf("http://%s/search/?query=%s&pass=%s", c.Host, query, c.Pass))
+	if err != nil {
+		return nil, err
+	}
+
+	trackResults := &SpotigoSearchTracks{}
+
+	err = unmarshal(searchJSON, trackResults)
+	if err != nil {
+		return nil, err
+	}
+
+	return trackResults, nil
+}
+
 func (c *Client) GetTrackInfo(url string) (*Track, error) {
 	regex := regexp.MustCompile("^(https:\\/\\/open.spotify.com\\/track\\/|spotify:track:)([a-zA-Z0-9]+)(.*)$")
 	trackID := regex.FindStringSubmatch(url)
@@ -55,34 +102,36 @@ func (c *Client) GetTrackInfo(url string) (*Track, error) {
 		return nil, errors.New("error finding track ID")
 	}
 
-	trackJSON, err := http.Get(fmt.Sprintf("http://%s/track/%s?pass=%s", c.Host, trackID[len(trackID)-2], c.Pass))
+	data := &Track{}
+	data.TrackID = trackID[len(trackID)-2]
+
+	trackJSON, err := http.Get(fmt.Sprintf("http://%s/track/%s?pass=%s", c.Host, data.TrackID, c.Pass))
 	if err != nil {
-		return nil, err
+		return data, err
 	}
-	trackOEmbedJSON, err := http.Get(fmt.Sprintf("https://embed.spotify.com/oembed?url=spotify:track:%s", trackID[len(trackID)-2]))
+	trackOEmbedJSON, err := http.Get(fmt.Sprintf("https://embed.spotify.com/oembed?url=spotify:track:%s", data.TrackID))
 	if err != nil {
-		return nil, err
+		return data, err
 	}
 
 	trackInfo := &SpotigoTrackInfo{}
 	err = unmarshal(trackJSON, trackInfo)
 	if err != nil {
-		return nil, errors.New("error unmarshalling track info")
+		return data, fmt.Errorf("%v", trackInfo)
 	}
 	if trackInfo.Name == "" {
-		return nil, errors.New("error getting track info")
+		return data, errors.New("error getting track info")
 	}
 
 	trackOEmbed := &SpotigoTrackOEmbed{}
 	err = unmarshal(trackOEmbedJSON, trackOEmbed)
 	if err != nil {
-		return nil, fmt.Errorf("%v", trackOEmbedJSON)
+		return data, fmt.Errorf("%v", trackOEmbedJSON)
 	}
 	if trackOEmbed.ThumbnailURL == "" {
-		return nil, errors.New("error getting track thumbnail")
+		return data, errors.New("error getting track thumbnail")
 	}
 
-	data := &Track{}
 	data.Artist = trackInfo.Album.Artist[0].Name
 	if len(trackInfo.Album.Artist) > 1 {
 		data.Artist += " ft. " + trackInfo.Album.Artist[1].Name
@@ -92,9 +141,8 @@ func (c *Client) GetTrackInfo(url string) (*Track, error) {
 			}
 		}
 	}
-	data.StreamURL = fmt.Sprintf("http://%s/download/%s?pass=%s", c.Host, trackID[len(trackID)-2], c.Pass)
+	data.StreamURL = fmt.Sprintf("http://%s/download/%s?pass=%s", c.Host, data.TrackID, c.Pass)
 	data.Title = trackInfo.Name
-	data.TrackID = trackID[len(trackID)-2]
 	data.Duration = trackInfo.Duration
 	data.ArtURL = trackOEmbed.ThumbnailURL
 
